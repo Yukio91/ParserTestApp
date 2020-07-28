8da009ed845a0527a5219a35361b3a6a3ba7f41b
Andrey <Andrey@LAPTOP-400QM9OM>
2020-07-28 18:02:22 +0500

    Add project files.

---
+++ ParserTestApp/Classes/Car.cs
@@ -0,0 +1,33 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParserTestApp.Classes
+{
+    public class Car
+    {
+        public string Vin { get; set; }
+        public string Price { get; set; }
+        public string CoverPhotoUrl { get; set; }
+        public override string ToString()
+        {
+            return $"Vin: {Vin} Price: {Price} CoverLink: {CoverPhotoUrl}";
+        }
+    }
+
+    public static class CarManager
+    {
+        public static Car ParseCarInfo(HtmlNode selectedNode)
+        {
+            return new Car()
+            {
+                Vin = selectedNode.SelectSingleNode("//dl[@class='vin']/dd")?.InnerText,
+                Price = selectedNode.SelectSingleNode("//span[@class='internetPrice final-price']/span[@class='value']")?.InnerText,
+                CoverPhotoUrl = selectedNode.SelectSingleNode("//img[@class='photo thumb']")?.Attributes.FirstOrDefault(attr => attr.Name == "src")?.Value
+            };
+        }
+    }
+}
---
+++ ParserTestApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using HtmlAgilityPack;
+using ParserTestApp.Classes;
+using System;
+using System.Linq;
+
+namespace ParserTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = @"https://www.kellysubaru.com/used-inventory/index.htm";
+
+
+            try
+            {
+                var htmlDoc = new HtmlWeb().Load(url);
+
+                var inventoryList = htmlDoc.DocumentNode.SelectSingleNode("//ul[@class='inventoryList data full list-unstyled']")?.ChildNodes.Where(li => li.Name == "li");
+                if (!(inventoryList?.Any() ?? false))
+                    return;
+
+                var selectedNode = inventoryList.Skip(1).FirstOrDefault();
+                if (selectedNode == null)
+                    return;
+
+                var car = CarManager.ParseCarInfo(selectedNode);
+
+                Console.WriteLine(car);
+                Console.ReadKey();
+            }
+            catch (HtmlWebException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+    }
+}
